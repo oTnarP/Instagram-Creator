835d9ddeae2bb6783365e6c9a875bfd53b3c7667
Sabbir Hossain Pranto <otnarp007@gmail.com>
2019-09-05 12:59:07 +0600

    First Commot

---
+++ Main.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Instagram_Creator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.successLabel = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // successLabel
+            // 
+            this.successLabel.AutoSize = true;
+            this.successLabel.ForeColor = System.Drawing.Color.ForestGreen;
+            this.successLabel.Location = new System.Drawing.Point(12, 241);
+            this.successLabel.MaximumSize = new System.Drawing.Size(250, 0);
+            this.successLabel.Name = "successLabel";
+            this.successLabel.Size = new System.Drawing.Size(0, 13);
+            this.successLabel.TabIndex = 25;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(12, 241);
+            this.errorLabel.MaximumSize = new System.Drawing.Size(250, 0);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "User ID:";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(15, 218);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(235, 20);
+            this.txtId.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Full Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(15, 171);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(235, 20);
+            this.txtName.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Email:";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(15, 124);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(235, 20);
+            this.txtMail.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Password:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(15, 77);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(235, 20);
+            this.txtPass.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Username:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(15, 30);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(235, 20);
+            this.txtUser.TabIndex = 14;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(15, 277);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(235, 23);
+            this.btnCreate.TabIndex = 13;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 310);
+            this.Controls.Add(this.successLabel);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnCreate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Instagram Creator - Softkini.com";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label successLabel;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,154 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace Instagram_Creator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                successLabel.Text = "";
+                errorLabel.Text = "";
+                /// <summary>Setup the HttpClient, HttpHandler and CookieContainer</summary>
+                CookieContainer instaCookies = new CookieContainer();
+                using (HttpClientHandler Handler = new HttpClientHandler() { CookieContainer = instaCookies, UseCookies = true, AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip })
+                using (HttpClient Client = new HttpClient(Handler))
+                {
+                    /// <summary>Set the referer header for our requests as Instragram checks to see if it exists</summary>
+                    Client.DefaultRequestHeaders.Add("referer", "https://www.instagram.com/");
+
+                    /// <summary>Set up the post data for when we send the account creation dry run to instagram</summary>
+                    Dictionary<string, string> instagramPostData = new Dictionary<string, string>
+            {
+                { "email", txtMail.Text },
+                { "password", txtPass.Text },
+                { "username", txtUser.Text },
+                { "first_name", txtName.Text }
+            };
+
+                    /// <summary>Make a GET request to Instagrams login page to set the site cookies inside our cookie container</summary>
+                    HttpRequestMessage getInstaMsg = new HttpRequestMessage() { Method = HttpMethod.Get, RequestUri = new Uri("https://www.instagram.com/accounts/login/") };
+                    HttpResponseMessage getInstaResponse = await Client.SendAsync(getInstaMsg);
+                    string instaResponseString = await getInstaResponse.Content.ReadAsStringAsync();
+
+                    /// <summary>Retrieve the csrftoken cookie from the cookie container and set it as a header as Instragram checks this too</summary>
+                    IEnumerable<Cookie> responseCookies = Handler.CookieContainer.GetCookies(new Uri("https://www.instagram.com")).Cast<Cookie>();
+                    Client.DefaultRequestHeaders.Add("x-csrftoken", responseCookies.FirstOrDefault(x => x.Name == "csrftoken").Value);
+
+                    /// <summary>Make a POST request to Instagrams dry run account creation endpoint</summary>
+                    HttpRequestMessage postInstaCheck = new HttpRequestMessage() { Method = HttpMethod.Post, RequestUri = new Uri("https://www.instagram.com/accounts/web_create_ajax/attempt/"), Content = new FormUrlEncodedContent(instagramPostData) };
+                    getInstaResponse = await Client.SendAsync(postInstaCheck);
+
+                    /// <summary>Deserialze the JSON response from Instragram in to a Object</summary>
+                    instagramRootObject instagramResponse = JsonConvert.DeserializeObject<instagramRootObject>(await getInstaResponse.Content.ReadAsStringAsync());
+
+                    /// <summary>Return a valid response if the dry run passed else return a invalid response</summary>
+                    if (instagramResponse.dryrun_passed)
+                    {
+                        /// <summary>Now we set the 'seamless login' parameter for the actual account creation request</summary>
+                        instagramPostData.Add("seamless_login_enabled", "0");
+
+                        /// <summary>Make a POST request to Instagrams account creation endpoint</summary>
+                        postInstaCheck = new HttpRequestMessage() { Method = HttpMethod.Post, RequestUri = new Uri("https://www.instagram.com/accounts/web_create_ajax/"), Content = new FormUrlEncodedContent(instagramPostData) };
+                        getInstaResponse = await Client.SendAsync(postInstaCheck);
+
+                        /// <summary>Deserialze the JSON response from Instragram in to a Object</summary>
+                        instagramResponse = JsonConvert.DeserializeObject<instagramRootObject>(await getInstaResponse.Content.ReadAsStringAsync());
+
+                        if (instagramResponse.account_created)
+                        {
+                            /// <summary>Read success response from Instragram and set 'user_id' field to 'userIdText' textbox text field</summary>
+                            successLabel.Text = $"Success: Account '{instagramPostData["username"]}' created";
+                            txtId.Text = instagramResponse.user_id;
+                        }
+                        else
+                        {
+                            /// <summary>Read errors from Instagrams response and set 'errorLabel' labels text to the error</summary>
+                            if (instagramResponse.errors != null)
+                            {
+                                if (instagramResponse.errors.ip != null)
+                                    errorLabel.Text = $"Error: Instragram is detecting current IP as proxy and denying registration";
+                                else if (instagramResponse.errors.email != null)
+                                    errorLabel.Text = $"Error: {instagramResponse.errors.email[0].code.Replace("_", " ")}";
+                                else if (instagramResponse.errors.password != null)
+                                    errorLabel.Text = $"Error: {instagramResponse.errors.password[0].code.Replace("_", " ")}";
+                                else if (instagramResponse.errors.username != null)
+                                    errorLabel.Text = $"Error: {instagramResponse.errors.username[0].code.Replace("_", " ")}";
+                                else
+                                    errorLabel.Text = "Error: Unknown error occured";
+                            }
+                            else
+                                errorLabel.Text = "Error: Unknown error occured";
+                        }
+                    }
+                    else
+                    {
+                        /// <summary>Read errors from Instagrams response and set 'errorLabel' labels text to the error</summary>
+                        if (instagramResponse.errors != null)
+                        {
+                            if (instagramResponse.errors.ip != null)
+                                errorLabel.Text = $"Error: Instragram is detecting current IP as proxy and denying registration";
+                            else if (instagramResponse.errors.email != null)
+                                errorLabel.Text = $"Error: {instagramResponse.errors.email[0].code.Replace("_", " ")}";
+                            else if (instagramResponse.errors.password != null)
+                                errorLabel.Text = $"Error: {instagramResponse.errors.password[0].code.Replace("_", " ")}";
+                            else if (instagramResponse.errors.username != null)
+                                errorLabel.Text = $"Error: {instagramResponse.errors.username[0].code.Replace("_", " ")}";
+                            else
+                                errorLabel.Text = "Error: Unknown error occured";
+                        }
+                        else
+                            errorLabel.Text = "Error: Unknown error occured";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                /// <summary>Gets any program errors and set 'errorLabel' labels text to the error</summary>
+                errorLabel.Text = $"Program Error: {ex.Message}";
+            }
+        }
+    }
+
+    public class errorMessage
+    {
+        public string message { get; set; }
+        public string code { get; set; }
+    }
+
+    public class Errors
+    {
+        public List<string> ip { get; set; }
+        public List<errorMessage> email { get; set; }
+        public List<errorMessage> username { get; set; }
+        public List<errorMessage> password { get; set; }
+    }
+
+    public class instagramRootObject
+    {
+        public bool account_created { get; set; }
+        public string user_id { get; set; }
+        public Errors errors { get; set; }
+        public bool dryrun_passed { get; set; }
+        public List<object> username_suggestions { get; set; }
+        public string status { get; set; }
+        public string error_type { get; set; }
+    }
+}
